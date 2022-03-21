4ead323b962b653e2fc422065d17900d7c7e18db
Zhang Yiwei <zywvvd@mail.ustc.edu.cn>
2022-03-21 17:14:38 +0800

    update-TI-value

--- lib/changdian.py
+++ lib/changdian.py
@@ -36,6 +36,8 @@ class ChangdianAoiInfo(KlarfInfo):
             'MPS-ST9430R18-M1': 3.,
             'MPS-ST9430R18-UBM': 3.,
             'TI-CD3701C0AYCUR(MHC0AECP)-M1': 1.5,
+            'TI-CD3722A2YCUR-UBM': 3.,
+            'TI-CD3701C0AYCUR(RFC0AECP)-BALLDROP': 1.5,
 
             ## mixed with DF
             'MPS-SM3519ZR8-UBM': 3.,
